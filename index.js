217e810550d57fbcc26c56adf8d3e088b90db997
Shawath <shakawathhossain0@gmail.com>
2026-08-30 12:06:06 +0600

    feat: implement verifyToken middleware for private routes

--- index.js
+++ index.js
@@ -30,6 +30,23 @@ const client = new MongoClient(uri, {
   }
 });
 
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
+  
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized access: No token provided' });
+  }
+  
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access: Invalid token' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 let useMongo = false;
 let database;
 let artifactsCollection;
